e75821cfc0dfca6a21268bf94e1670a1e32d24ba
Saji Ayahito <ayahito_saji@outlook.jp>
2020-04-18 15:22:32 +0900

    トップページモックの作成

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,95 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div style={{
+      width: '100vw',
+      height: '100vh',
+      backgroundImage: 'url(/background2.jpg)',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      overflow: 'auto'
+    }}>
+      <div style={{
+        paddingTop: '100px',
+        paddingBottom: '100px',
+        width: '640px',
+        maxWidth: '90%',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        <div style={{
+          color: '#ffffff',
+          marginTop: '150px'
+        }}>
+          <div style={{fontSize: '16pt'}}>
+            居酒屋
+          </div>
+          <div style={{fontSize: '32pt'}}>
+            ひきこもり
+          </div>
+        </div>
+        <Link to="waiting" style={{
+          marginTop: '100px',
+          color: '#ffffff',
+          border: 'none',
+          padding: '24px',
+          borderRadius: '48px',
+          fontSize: '16px',
+          textDecoration: 'none',
+          background: 'rgba(0, 0, 0, 0.7)'
+        }}>
+          飲み会を始める
+        </Link>
+        <hr color='#ffffff' style={{
+          width: '100%',
+          marginTop: '100px'
+        }} />
+        <h1 style={{
+          color: '#ffffff',
+          marginTop: '100px'
+        }}>
+          いつでもどこでも，オンライン飲み会
+        </h1>
+        <p style={{
+          color: '#ffffff',
+          marginTop: '50px',
+          width: '100%'
+        }}>
+          ここに説明文ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div style={{
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        width: '100vw',
+        height: '100px',
+        background: 'rgba(0, 0, 0, 0.7)',
+        color: 'rgba(255, 255, 255)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+      </div>
+      <div style={{
+        position: 'absolute',
+        bottom: '0',
+        left: '0',
+        width: '100vw',
+        height: '100px',
+        background: 'rgba(0, 0, 0, 0.7)',
+        color: 'rgba(255, 255, 255)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+      </div>
     </div>
   );
 }
